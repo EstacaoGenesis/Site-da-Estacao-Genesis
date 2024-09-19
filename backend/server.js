edbfbe41d85014a98a307ac3a74a4fa0c624b214
Kauakim <kauasantarita@gmail.com>
2024-09-19 00:09:46 -0300

    Conexao do backend com o SQL realizada

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,28 @@
+//Importa as ferramentas utilizadas pelo codigo
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
 require('dotenv').config();
 
 
+// Inicia a aplicação na porta escolhida
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
-// Conexão com o banco de dados 'usuarios'
+// Define a conexão do backend da aplicação com os três bancos de dados do projeto 
 const dbUsuarios = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE_USUARIOS
 });
-
-// Conexão com o banco de dados 'historico'
 const dbHistorico = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE_HISTORICO
 });
-
-// Conexão com o banco de dados 'previsao'
 const dbPrevisao = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -33,37 +31,33 @@ const dbPrevisao = mysql.createConnection({
 });
 
 
-// Função para conectar ao banco de dados 'usuarios'
+// Executa a conexão do backend da aplicação com os três bancos de dados do projeto
 dbUsuarios.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar no banco de dados usuarios: ', err);
+    console.error('Erro ao conectar no banco de dados "site": ', err);
   } else {
-    console.log('Conectado ao banco de dados usuarios');
+    console.log('Conectado ao banco de dados "site"');
   }
 });
-
-// Função para conectar ao banco de dados 'previsao'
 dbPrevisao.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar no banco de dados previsao: ', err);
+    console.error('Erro ao conectar no banco de dados "previsao": ', err);
   } else {
-    console.log('Conectado ao banco de dados previsao');
+    console.log('Conectado ao banco de dados "previsao"');
   }
 });
-
-// Função para conectar ao banco de dados 'historico'
 dbHistorico.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar no banco de dados historico: ', err);
+    console.error('Erro ao conectar no banco de dados "historico": ', err);
   } else {
-    console.log('Conectado ao banco de dados historico');
+    console.log('Conectado ao banco de dados "historico"');
   }
 });
 
 
-// Endpoints
+// Endpoints com todas as informações dos três bancos de dados do projeto
 app.get('/usuarios', (req, res) => {
-  dbUsuarios.query('SELECT * FROM usuarios', (err, results) => {
+  dbUsuarios.query('SELECT * FROM data', (err, results) => {
     if (err) {
       res.status(500).send('Erro ao buscar dados dos usuários.');
       return;
@@ -71,9 +65,8 @@ app.get('/usuarios', (req, res) => {
     res.json(results);
   });
 });
-
 app.get('/historico', (req, res) => {
-  dbHistorico.query('SELECT * FROM historico', (err, results) => {
+  dbHistorico.query('SELECT * FROM main', (err, results) => {
     if (err) {
       res.status(500).send('Erro ao buscar dados do histórico.');
       return;
@@ -81,9 +74,8 @@ app.get('/historico', (req, res) => {
     res.json(results);
   });
 });
-
 app.get('/previsao', (req, res) => {
-  dbPrevisao.query('SELECT * FROM previsao', (err, results) => {
+  dbPrevisao.query('SELECT * FROM sensores', (err, results) => {
     if (err) {
       res.status(500).send('Erro ao buscar dados da previsão.');
       return;
@@ -93,12 +85,12 @@ app.get('/previsao', (req, res) => {
 });
 
 
-// Servir arquivos estáticos
+// Inicia os arquivos estáticos do projeto
 app.use('/assets', express.static(path.join(__dirname, '../assets')));
 app.use(express.static(path.join(__dirname, '../templates')));
 
 
-//node server.js  ou  nodemon server.js 
+// Inicia o backend (node server.js  ou  nodemon server.js)
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
